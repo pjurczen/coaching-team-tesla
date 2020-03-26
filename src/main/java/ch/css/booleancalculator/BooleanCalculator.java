2fd353c37d0ccf12c8eacf2d44e07a9b51841d92
Piotr Jurczenko <16348752+pjurczen@users.noreply.github.com>
2020-03-26 14:51:22 +0100

    F calculate boolean value for "T"

--- src/main/java/ch/css/booleancalculator/BooleanCalculator.java
+++ src/main/java/ch/css/booleancalculator/BooleanCalculator.java
@@ -1,4 +1,8 @@
 package ch.css.booleancalculator;
 
 public class BooleanCalculator {
+
+    public boolean evaluate(String given) {
+        return false;
+    }
 }
--- src/test/java/ch/css/booleancalculator/BooleanCalculatorTest.java
+++ src/test/java/ch/css/booleancalculator/BooleanCalculatorTest.java
@@ -2,10 +2,20 @@ package ch.css.booleancalculator;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class BooleanCalculatorTest {
 
-
-
+    @Test
+    public void dada() {
+        // given
+        String given = "T";
+        boolean expected = false;
+        // when
+        boolean actual = new BooleanCalculator().evaluate(given);
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
 
 }
 
@@ -13,12 +23,19 @@ public class BooleanCalculatorTest {
  *
  * ________________________________________________________________________________________________________
  * Boolean Calculator:  Entwickle einen Boolean Calculator. Er bekommt einen String als Input und wertet diesen String zu einem boolean aus.
- *  1.) einzelne Werte:  "TRUE" -> true "FALSE" -> false 
+ *
+ * 1.) einzelne Werte:  "TRUE" -> true "FALSE" -> false 
+ *
  * 2.) NOT Operator  "NOT TRUE" -> false 
+ *
  * 3.) AND Operator  "TRUE AND FALSE" -> false "TRUE AND TRUE" -> true 
+ *
  * 4.) OR Operator  "TRUE OR FALSE" -> true "FALSE OR FALSE" -> false 
+ *
  * 5.) Beliebige Anzahl von AND und OR Operatoren, NOT hat Vorrang vor AND und OR  "TRUE OR TRUE OR TRUE AND FALSE" -> true "TRUE OR FALSE AND NOT FALSE" -> true 
+ *
  * 6.) Klammern  "(TRUE OR TRUE OR TRUE) AND FALSE" -> false "NOT (TRUE AND TRUE)" -> false 
+ *
  * 7.) Drucke den abstrakten Syntax Baum aus, der die Berechnung repräsentiert  
  * "(TRUE OR TRUE OR TRUE) AND FALSE" ->  
  * AND 
