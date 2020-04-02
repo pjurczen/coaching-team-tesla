15566eb7457ab3a00177b0c6f6a8c809bcb1c36e
atomicburger <bylm@protonmail.com>
2020-04-02 15:55:45 +0200

    R Split eingefuehrt und angewendet

--- src/main/java/ch/css/booleancalculator/BooleanCalculator.java
+++ src/main/java/ch/css/booleancalculator/BooleanCalculator.java
@@ -10,15 +10,40 @@ public class BooleanCalculator {
 
     public boolean evaluate(String given) {
         boolean result = false;
-        if ((TRUE + S + AND + S + TRUE).equals(given)){
-            result = true;
+       
+        String[] elements = split(given);
+        
+        if (given.contains(AND)) {
+            result = evaluate(elements[0]) && evaluate(elements[2]); 
         }
-        if ((NOT + S + FALSE).equals(given)){
-            result = true;
+        if ((elements[0]).equals(NOT)){
+            result = ! evaluate(elements[1]);
         }
         if (TRUE.equals(given)) {
             result = true;
         }
         return result;
     }
+
+    String[] split(String given) {
+        return given.split(S);
+    }
 }
+
+/* **** TPP ******
+({} → nil) no code at all → code that employs nil
+(nil → constant)
+(constant → constant+) a simple constant to a more complex constant
+(constant → scalar) replacing a constant with a variable or an argument
+(statement → statements) adding more unconditional statements.
+(unconditional → if) splitting the execution path
+(scalar → array)
+(array → container)
+(statement → tail-recursion)
+(if → while)
+(statement → non-tail-recursion)
+(expression → function) replacing an expression with a function or algorithm
+(variable → assignment) replacing the value of a variable.
+(case) adding a case (or else) to an existing switch or if
+
+*/
--- src/test/java/ch/css/booleancalculator/BooleanCalculatorTest.java
+++ src/test/java/ch/css/booleancalculator/BooleanCalculatorTest.java
@@ -1,25 +1,26 @@
 package ch.css.booleancalculator;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class BooleanCalculatorTest {
 
     @ParameterizedTest
     @CsvSource({
-            "T, false",
-            "TR, false",
-            "TRU, false",
-            "TRUE, true",
-            "FALSE, false",
-            "NOT FALSE, true",
-            "NOT TRUE, false",
-            "FALSE AND FALSE, false",
-            "FALSE AND TRUE, false",
-            "TRUE AND FALSE, false",
-            "TRUE AND TRUE, true",
+        "T, false",
+        "TR, false",
+        "TRU, false",
+        "TRUE, true",
+        "FALSE, false",
+        "NOT FALSE, true",
+        "NOT TRUE, false",
+        "FALSE AND FALSE, false",
+        "FALSE AND TRUE, false",
+        "TRUE AND FALSE, false",
+        "TRUE AND TRUE, true",
     })
     public void evaluateBooleanFromString(String givenString, boolean expectedBoolean) {
         // given
@@ -28,6 +29,28 @@ public class BooleanCalculatorTest {
         // then
         assertThat(actual).isEqualTo(expectedBoolean);
     }
+
+    @Test
+    public void shouldSplitFalse() {
+        //arrange
+        String given = "FALSE";
+        String[] expectedElements = { "FALSE" };
+        //act
+        String[] actual = new BooleanCalculator().split(given);
+        // assert
+        assertThat(actual).isEqualTo(expectedElements);
+    }
+
+    @Test
+    public void shouldSplitNotFalse() {
+        // arrange
+        String given = "NOT FALSE";
+        String[] expectedElements = { "NOT", "FALSE" };
+        // act
+        String[] actual = new BooleanCalculator().split(given);
+        // assert
+        assertThat(actual).isEqualTo(expectedElements);
+    }
 }
 
 /**
@@ -35,36 +58,36 @@ public class BooleanCalculatorTest {
  * ________________________________________________________________________________________________________
  * Boolean Calculator:  Entwickle einen Boolean Calculator. Er bekommt einen String als Input und wertet diesen String zu einem boolean aus.
  *
- * 1.) einzelne Werte:  "TRUE" -> true "FALSE" -> false 
+ * 1.) einzelne Werte:  "TRUE" -> true "FALSE" -> false
  *
- * 2.) NOT Operator  "NOT TRUE" -> false 
+ * 2.) NOT Operator  "NOT TRUE" -> false
  *
- * 3.) AND Operator  "TRUE AND FALSE" -> false "TRUE AND TRUE" -> true 
+ * 3.) AND Operator  "TRUE AND FALSE" -> false "TRUE AND TRUE" -> true
  *
- * 4.) OR Operator  "TRUE OR FALSE" -> true "FALSE OR FALSE" -> false 
+ * 4.) OR Operator  "TRUE OR FALSE" -> true "FALSE OR FALSE" -> false
  *
- * 5.) Beliebige Anzahl von AND und OR Operatoren, NOT hat Vorrang vor AND und OR  "TRUE OR TRUE OR TRUE AND FALSE" -> true "TRUE OR FALSE AND NOT FALSE" -> true 
+ * 5.) Beliebige Anzahl von AND und OR Operatoren, NOT hat Vorrang vor AND und OR  "TRUE OR TRUE OR TRUE AND FALSE" -> true "TRUE OR FALSE AND NOT FALSE" -> true
  *
- * 6.) Klammern  "(TRUE OR TRUE OR TRUE) AND FALSE" -> false "NOT (TRUE AND TRUE)" -> false 
+ * 6.) Klammern  "(TRUE OR TRUE OR TRUE) AND FALSE" -> false "NOT (TRUE AND TRUE)" -> false
  *
- * 7.) Drucke den abstrakten Syntax Baum aus, der die Berechnung repräsentiert  
- * "(TRUE OR TRUE OR TRUE) AND FALSE" ->  
- * AND 
+ * 7.) Drucke den abstrakten Syntax Baum aus, der die Berechnung repräsentiert
+ * "(TRUE OR TRUE OR TRUE) AND FALSE" ->
+ * AND
  * |_OR |
  * |_TRUE
- * ||_OR 
+ * ||_OR
  * | |_TRUE
- * | |_TRUE 
- * |_FALSE  
+ * | |_TRUE
+ * |_FALSE
  *
  * "NOT ((TRUE OR TRUE) OR (TRUE AND FALSE))" ->
  *
  *  NOT
- * |_OR 
+ * |_OR
  *  |_OR  
- *  |  |_TRUE 
- *  |  |_TRUE 
- *  |_AND 
- *     |_TRUE 
- *     |_FALSE 
+ *  |  |_TRUE
+ *  |  |_TRUE
+ *  |_AND
+ *     |_TRUE
+ *     |_FALSE
  **/
